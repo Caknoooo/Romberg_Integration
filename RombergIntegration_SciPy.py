8b9e484d301782b973374325a3dda6d447d81a09
Caknoooo <caknocomel@gmail.com>
2022-12-20 22:48:09 +0700

    feat: added RombergIntegration using SciPy library

---
+++ RombergIntegration_SciPy.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import integrate
+
+# misal mau mencari integral dari [0 - 2] dari ex^2 + 2x
+def func(x):
+    return np.exp(x**2) + 2*x
+
+# Menggunakan fungsi bawaan scipy yaitu integrasi romberg
+romberg = integrate.romberg(func, 0, 2, show=True)
+
+print(romberg)
